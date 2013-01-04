a238158b54037b9f1e67c43473826ee321bda560
Jake Spurlock <whyisjake@gmail.com>
2013-01-04 12:46:09 -0800

    Getting rid of the content, and moving it into the index file.

--- index.php
+++ index.php
@@ -8,25 +8,60 @@
 
 		<div class="content span8">
 
-			<?php
-						if ( have_posts() ) :
-							while ( have_posts() ) :
-								the_post();
-								get_template_part( 'content' );
-							endwhile; ?>
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+				
+				<article <?php post_class(); ?>>
 
-							<ul class="pager">
-						
-								<li class="previous"><?php previous_posts_link('&larr; Previous Page'); ?></li>
-								<li class="next"><?php next_posts_link('Next Page &rarr;'); ?></li>
+					<!--<p class="categories"><?php the_category(', '); ?></p>-->
+
+					<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+
+					<p class="meta top">By <?php the_author_posts_link(); ?>, <?php the_time('Y/m/d \@ g:i a') ?></p>
+
+					<?php the_content(); ?>
+
+					<div class="clear"></div>
+
+					<div class="row">
+
+						<div class="postmeta">
+
+							<div class="span-thumb thumbnail">
 							
-							</ul>
+								<?php echo get_avatar( get_the_author_meta('user_email'), 72); ?>
+							
+							</div>
+							
+							<div class="span-well well">
+							
+								<p>Posted by <?php the_author_posts_link(); ?> | <a href="<?php the_permalink(); ?>"><?php the_time('l F jS, Y g:i A'); ?></a></p>
+								<p>Categories: <?php the_category(', '); ?> | <?php comments_popup_link(); ?> <?php edit_post_link('Fix me...', ' | '); ?></p>
+
+							</div>
+
+						</div>
+
+						<div class="clear"></div>
 						
-						<?php else: ?>
-						
-							<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
-						
-						<?php endif; ?>
+					</div>
+				
+				</article>
+
+							
+			<?php endwhile; ?>
+
+				<ul class="pager">
+			
+					<li class="previous"><?php previous_posts_link('&larr; Previous Page'); ?></li>
+					<li class="next"><?php next_posts_link('Next Page &rarr;'); ?></li>
+				
+				</ul>
+			
+			<?php else: ?>
+			
+				<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+			
+			<?php endif; ?>
 
 		</div><!--Content-->
 
