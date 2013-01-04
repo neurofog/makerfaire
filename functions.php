44370489de965ce2f70cc1258e80068585cb7d7f
Jake Spurlock <whyisjake@gmail.com>
2013-01-04 11:25:06 -0800

    Adding the news shortcode

--- functions.php
+++ functions.php
@@ -9,3 +9,13 @@ function make_enqueue_jquery() {
 }
 
 add_action( 'wp_enqueue_scripts', 'make_enqueue_jquery' );
+
+
+function makerfaire_get_news() {
+
+	$url = 'http://blog.makezine.com/maker-faire-news/';
+	$output = wpcom_vip_file_get_contents( $url, 3, 60*60,  array( 'obey_cache_control_header' => false ) );
+	return $output;
+}
+
+add_shortcode('news', 'makerfaire_get_news');
