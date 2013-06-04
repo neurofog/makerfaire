41c193de9bc4d14165cab1eb6274af1a7936a443
Jake Spurlock <whyisjake@gmail.com>
2013-06-04 14:09:26 -0700

    New merged tags and categories.

--- plugins/maker-faire-forms/forms/tags.php
+++ plugins/maker-faire-forms/forms/tags.php
@@ -1,62 +1,17 @@
 <div style="clear:both; height:20px;"></div>
-<div class="tag-cluster">
-    <h3><input name="cat[]" type="checkbox" value="electronics" <?php checked(in_array('electronics', $this->form['cats'])); ?> /> &nbsp; Electronics</h3>
-     <?php 
-	 	$cats = get_categories(array('hide_empty'=>false, 'parent'=>7334));
-	 	foreach($cats as $cat) : ?>
-   			<div><input name="cat[]" type="checkbox" value="<?php echo esc_attr($cat->slug); ?>" <?php checked(in_array($cat->slug, $this->form['cats'])); ?> /> &nbsp; <?php echo esc_html($cat->name); ?></div>
-    <?php endforeach; ?>
-    
-    <h3><input name="cat[]" type="checkbox" value="workshop" <?php checked(in_array('workshop', $this->form['cats'])); ?> /> &nbsp; Workshop</h3>
-    <?php 
-	 	$cats = get_categories(array('hide_empty'=>false, 'parent'=>19557));
-	 	foreach($cats as $cat) : ?>
-   			<div><input name="cat[]" type="checkbox" value="<?php echo esc_attr($cat->slug); ?>" <?php checked(in_array($cat->slug, $this->form['cats'])); ?> /> &nbsp; <?php echo esc_html($cat->name); ?></div>
-    <?php endforeach; ?>
-    
-    <h3><input name="cat[]" type="checkbox" value="craft" <?php checked(in_array('craft', $this->form['cats'])); ?> /> &nbsp; Craft</h3>
-    <?php 
-	 	$cats = get_categories(array('hide_empty'=>false, 'parent'=>14877));
-	 	foreach($cats as $cat) : ?>
-   			<div><input name="cat[]" type="checkbox" value="<?php echo esc_attr($cat->slug); ?>" <?php checked(in_array($cat->slug, $this->form['cats'])); ?> /> &nbsp; <?php echo esc_html($cat->name); ?></div>
-    <?php endforeach; ?>
-    
-    <h3><input name="cat[]" type="checkbox" value="science" <?php checked(in_array('science', $this->form['cats'])); ?> /> &nbsp; Science</h3>
-    <?php 
-	 	$cats = get_categories(array('hide_empty'=>false, 'parent'=>173));
-	 	foreach($cats as $cat) : ?>
-   			<div><input name="cat[]" type="checkbox" value="<?php echo esc_attr($cat->slug); ?>" <?php checked(in_array($cat->slug, $this->form['cats'])); ?> /> &nbsp; <?php echo esc_html($cat->name); ?></div>
-    <?php endforeach; ?>
-
-	<h3><input name="cat[]" type="checkbox" value="home" <?php checked(in_array('home', $this->form['cats'])); ?> /> &nbsp; Home</h3>
-    <?php 
-	 	$cats = get_categories(array('hide_empty'=>false, 'parent'=>400));
-	 	foreach($cats as $cat) : ?>
-   			<div><input name="cat[]" type="checkbox" value="<?php echo esc_attr($cat->slug); ?>" <?php checked(in_array($cat->slug, $this->form['cats'])); ?> /> &nbsp; <?php echo esc_html($cat->name); ?></div>
-    <?php endforeach; ?>
-    
-    <h3><input name="cat[]" type="checkbox" value="art-design" <?php checked(in_array('art-design', $this->form['cats'])); ?> /> &nbsp; Art & Design</h3>
-    <?php 
-	 	$cats = get_categories(array('hide_empty'=>false, 'parent'=>2806));
-	 	foreach($cats as $cat) : ?>
-   			<div><input name="cat[]" type="checkbox" value="<?php echo esc_attr($cat->slug); ?>" <?php checked(in_array($cat->slug, $this->form['cats'])); ?> /> &nbsp; <?php echo esc_html($cat->name); ?></div>
-    <?php endforeach; ?>
-
-    <h3><input name="cat[]" type="checkbox" value="pro" <?php checked(in_array('pro', $this->form['cats'])); ?> /> &nbsp; Pro</h3>
-    <?php 
-	 	$cats = get_categories(array('hide_empty'=>false, 'parent'=>78436));
-	 	foreach($cats as $cat) : ?>
-   			<div><input name="cat[]" type="checkbox" value="<?php echo esc_attr($cat->slug); ?>" <?php checked(in_array($cat->slug, $this->form['cats'])); ?> /> &nbsp; <?php echo esc_html($cat->name); ?></div>
-    <?php endforeach; ?>
-</div>
-
-<div class="tag-cluster">
-    <?php 
-		$tags = get_tags(array('hide_empty'=>false));
-		foreach ($tags as $tag) : ?>
-    	<div><input name="tag[]" type="checkbox" value="<?php echo esc_attr($tag->slug); ?>" <?php checked(in_array($tag->slug, $this->form['tags'])); ?> /> &nbsp; <?php echo esc_html($tag->name); ?></div>
-    <?php endforeach; ?>
-</div>
-
+<ul class="columns">
+	<?php
+		$terms = get_terms( array( 'category', 'post_tag' ), array('hide_empty' => 0, ) );
+		if (!empty($terms)) {
+			foreach ($terms as $idx => $term) {
+				if ( $term->taxonomy == 'post_tag' ) { ?>
+					<label class="checkbox"><input name="tag[]" type="checkbox" value="<?php echo esc_attr($term->slug); ?>" <?php checked( in_array( $term->slug, $this->form['tags'] ) ); ?> /> <?php echo esc_html($term->name); ?></label>
+				<?php } else { ?>
+					<label class="checkbox"><input name="cat[]" type="checkbox" value="<?php echo esc_attr($term->slug); ?>" <?php checked( in_array( $term->slug, $this->form['cats'] ) ); ?> /> <?php echo esc_html($term->name); ?></label>
+				<?php } ?>
+			<?php }
+		}
+	?>
+</ul>
 
 <div class="clear" style="height:30px;"></div>
