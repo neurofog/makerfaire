f84eea66e36846bed6a5605f65340775736f317d
Cole Geissinger <cgeissinger@makermedia.com>
2013-06-06 17:24:57 -0700

    Early copy of the new Maker extraction CLI script. Still need to implement the post insert function and actually move them.

--- plugins/wp-cli/wp-cli.php
+++ plugins/wp-cli/wp-cli.php
@@ -31,7 +31,7 @@ class MAKE_CLI extends WP_CLI_Command {
 		$query = new WP_Query( $args );
 
 		while ( $query->have_posts() ) : $query->the_post();
-		global $post;
+			global $post;
 			setup_postdata($post);
 			WP_CLI::line( get_the_title() );
 			$json_post = json_decode( str_replace( "\'", "'", get_the_content() ) );
@@ -138,6 +138,7 @@ class MAKE_CLI extends WP_CLI_Command {
 	 */
 	public function mf_create_makers( $args, $assoc_args ) {
 
+		$faire_slug = 'maker-faire-bay-area-2013';
 		$args = array(
 			'posts_per_page' => 2000,
 			'post_type' => 'mf_form',
@@ -146,6 +147,15 @@ class MAKE_CLI extends WP_CLI_Command {
 			// Prevent new posts from affecting the order
 			'orderby' => 'ID',
 			'order' => 'ASC',
+
+			// Get our latest applications in the "faire" taxonomy
+			'tax_query' => array(
+				array(
+					'taxonomy' => 'faire',
+					'field' => 'slug',
+					'terms' => $faire_slug,
+				)
+			),
 
 			// Speed this up
 			'no_found_rows' => true,
@@ -156,17 +166,162 @@ class MAKE_CLI extends WP_CLI_Command {
 		// Get the first set of posts
 		$query = new WP_Query( $args );
 
+		if ( isset( $query ) && ! empty( $query ) )
+			WP_CLI::line( 'Fetching Latest Maker Faire Appliactions...' );
+
+
+		// Do this.
 		while ( $query->have_posts() ) : $query->the_post();
 		
-		global $post;
+			global $post;
 			
-		setup_postdata( $post );
-
+			setup_postdata( $post );
 		
+			// Get our applications
+			$app   = json_decode( html_entity_decode( $post->post_content ) );
+			$type  = ( isset( $app->form_type ) ) ? $app->form_type : 'null';
 
-		WP_CLI::line( '' );
+			// Use a switch to easily check for all the different application types
+			switch ( $type ) {
+
+				// If the application is an "exhibit", get the info we need.
+				case 'exhibit' :
+
+					// Check what kind of maker we have; a single, a list, or a group. Return their email.
+					switch ( $app->maker ) {
+
+						// A single maker
+						case 'One maker' :
+
+							$maker_type  = 'One Maker';
+							$maker_email = ( isset( $app->maker_email ) ) ? $app->maker_email : $app->email;
+							$maker_name  = ( isset( $app->maker_name ) ) ? $app->maker_name : $app->name;
+							$maker_photo = ( isset( $app->maker_photo ) ) ? $app->maker_photo: $app->project_photo;
+
+							break;
+
+						// A list of makers
+						case 'A list of makers' :
+
+							$maker_type  = 'A List of Makers';
+							$num_makers  = count( $app->m_maker_name );
+							$maker_email = array();
+							$maker_name  = array();
+							$maker_photo = array();
+							
+							// Let's loop through all of our makers in one shot shall we?
+							for ( $i = 0; $i < $num_makers; $i++ ) {
+								
+								// Get our maker email. Some applicants have empty fields, default to the main contact
+								if ( $app->m_maker_email[ $i ] != '' ) {
+									$maker_email[] = $app->m_maker_email[ $i ];
+								} else {
+									$maker_email = $app->email;
+								}
+
+								// Get our maker name. Some applicants have empty fields, default to the main contact
+								if ( $app->m_maker_name[ $i ] != '' ) {
+									$maker_name[] = $app->m_maker_name[ $i ];
+								} else {
+									$maker_name = $app->name;
+								}
+
+								// Get our maker photo. Again, Some applicants have empty fields, default to the main contact
+								if ( $app->m_maker_photo[ $i ] != '' ) {
+									$maker_photo = $app->m_maker_photo[ $i ];
+								} else {
+									$maker_photo = $app->project_photo;
+								}
+
+							}
+
+							break;
+
+						// A group of makers
+						case 'A group or association' :
+
+							$maker_type  = 'A Group or Association';
+							$maker_email = $app->email; // Since the group option doens't have a "Group Email" field, we'll just grab the main applicants emails
+							$maker_name  = $app->group_name;
+
+							break;
+					}
+
+					// Get our makers website URL
+					if ( isset( $app->project_website ) && ! empty( $app->project_website ) )
+						$maker_website = $app->project_website;
+
+					// Get our makers video URL
+					if ( isset( $app->project_video ) && ! empty( $app->project_video ) )
+						$maker_video = $app->project_video; 
+
+					break;
+
+			}
+
+
+			// Now that we have all of our delicious Maker info. Let's add them to our Maker Custom Post Type...
+			// First, we need to deal with the application with multiple makers. If an array exists, loop through them and add each as an individual post.
+			// Use email as our main key to pair and loop.
+			if ( is_array( $maker_email ) ) {
+
+				$num_makers = count( $maker_email );
+
+				// Loop through each maker
+				for ( $i = 0; $i < $num_makers; $i++ ) {
+
+					// Check if the maker already exists.
+					$maker = get_page_by_title( $maker_name[ $i ], OBJECT, 'maker' );
+
+					if ( ! $maker ) {
+						WP_CLI::success( 'User created' );
+					} else {
+						WP_CLI::success( 'User updated!' );
+					}
+
+					// Show the output of what we just did.
+					WP_CLI::line( ' | Exhibit' );
+					WP_CLI::line( ' | ID: ' . $post->ID );
+					WP_CLI::line( ' | TYPE: ' . $maker_type );
+					WP_CLI::line( ' | NAME: ' . $maker_name[ $i ] );
+					WP_CLI::line( ' | EMAIL: ' . $maker_email[ $i ] );
+					WP_CLI::line( ' | PHOTO: ' . $maker_photo[ $i ] );
+					WP_CLI::line( ' | WEBSITE: ' . $maker_website );
+					WP_CLI::line( ' | VIDEO: ' . $maker_video );
+					WP_CLI::line( ' | FAIRE: ' . $faire_slug );
+
+					// Leave some evidence we are done with this application.
+					WP_CLI::line( ' ----------------------------------------' );
+				}
+
+			} else {
+
+				// Check if the maker already exists.
+				$maker = get_page_by_title( $maker_name, OBJECT, 'maker' );
+
+				if ( ! $maker ) {
+					WP_CLI::success( 'User created' );
+				} else {
+					WP_CLI::success( 'User updated!' );
+				}
+
+				// Show the output of what we just did.
+				WP_CLI::line( ' | Exhibit' );
+				WP_CLI::line( ' | ID: ' . $post->ID );
+				WP_CLI::line( ' | TYPE: ' . $maker_type );
+				WP_CLI::line( ' | NAME: ' . $maker_name );
+				WP_CLI::line( ' | EMAIL: ' . $email );
+				WP_CLI::line( ' | PHOTO: ' . $maker_photo );
+				WP_CLI::line( ' | WEBSITE: ' . $maker_website );
+				WP_CLI::line( ' | VIDEO: ' . $maker_video );
+				WP_CLI::line( ' | FAIRE: ' . $faire_slug );
+
+				// Leave some evidence we are done with this application.
+				WP_CLI::line( ' ----------------------------------------' );
+			}
 		endwhile;
-		WP_CLI::success( "Boom!" );
+
+		WP_CLI::success( 'SHAZAM! Job is DONE. Get a beer! You deserve it big guy! ;)' );
 	}
 
 	/**
