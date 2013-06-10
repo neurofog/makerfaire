05349cb7858e20970aaf1ac275de95df5e11cd67
Jake Spurlock <whyisjake@gmail.com>
2013-06-10 12:07:01 -0700

    Just fooling around a bit here. Want to save, but start over.

--- plugins/status-board/status-board.php
+++ plugins/status-board/status-board.php
@@ -2,54 +2,39 @@
 /**
  * Get the amount of new posts over the last five days for pending and accepted Maker Faire Applications
  */
-function mf_get_post_count() {
-	$accepted = array();
+function mf_get_post_count( $post_types = array( 'mf_form', 'post' ), $status = array( 'pending', 'accepted' ) ) {
 	$output = array();
-	for ( $count = 0; $count <= 10; $count++ ) {
-		$query = wp_cache_get( 'today_posts_' . $count );
-		if ( $query == false ) {
-			$time = date( 'F jS, Y', strtotime( '-' . $count . ' days' ) );
-			$date = new DateTime( $time );
-			$args = array(
-				'year'			=> $date->format('Y'),
-				'monthnum' 		=> $date->format('m'),
-				'day'			=> $date->format('d'),
-				'post_type'		=> 'mf_form',
-				'post_status'	=> 'accepted'
-				);
-			$query = new WP_Query( $args );
-			// var_dump( $query->query );
-			wp_cache_set( 'today_posts_' . $count, $query, '', 300 );
-			$the = array( 'title' => $time, 'value' => $query->post_count );
-			array_push( $accepted, $the );
+	foreach ($post_types as $post_type ) {
+		$accepted = array();
+		for ( $count = 0; $count <= 10; $count++ ) {
+			$query = wp_cache_get( 'today_posts_' . $count );
+			if ( $query == false ) {
+				$time = date( 'F jS, Y', strtotime( '-' . $count . ' days' ) );
+				$date = new DateTime( $time );
+				$args = array(
+					'year'			=> $date->format('Y'),
+					'monthnum' 		=> $date->format('m'),
+					'day'			=> $date->format('d'),
+					'post_type'		=> $post_type,
+					'post_status'	=> 'accepted'
+					);
+				$query = new WP_Query( $args );
+				// var_dump( $query->query );
+				wp_cache_set( 'today_posts_' . $count, $query, '', 300 );
+				$the = array( 'title' => $time, 'value' => $query->post_count );
+				array_push( $accepted, $the );
+				$output[ $post_type ] = $accepted;
+			}
 		}
 	}
-	$pending = array();
-	for ( $count = 0; $count <= 10; $count++ ) {
-		$query = wp_cache_get( 'pending_today_posts_' . $count );
-		if ( $query == false ) {
-			$time = date( 'F jS, Y', strtotime( '-' . $count . ' days' ) );
-			$date = new DateTime( $time );
-			$args = array(
-				'year'			=> $date->format('Y'),
-				'monthnum' 		=> $date->format('m'),
-				'day'			=> $date->format('d'),
-				'post_type'		=> 'mf_form',
-				'post_status'	=> 'proposed'
-				);
-			$query = new WP_Query( $args );
-			wp_cache_set( 'pending_today_posts_' . $count, $query, '', 300 );
-			$the = array( 'title' => $time, 'value' => $query->post_count );
-			array_push( $pending, $the );
-		}
-	}
+	var_dump( $output );
 	$datasequences = array( 
 		array( 
-			'title' => 'Accepted', 
+			'title' => 'Accepted',
 			'datapoints' => $accepted
 		), array (
 			'title' => 'Pending', 
-			'datapoints' => $pending
+			// 'datapoints' => $pending
 		)
 	);
 	$output = $datasequences;
