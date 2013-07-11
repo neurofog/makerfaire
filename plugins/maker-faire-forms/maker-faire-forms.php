3291ea2b1d0abc1cabb4b6e1c905ba368af142a9
Jake Spurlock <whyisjake@gmail.com>
2013-07-10 17:01:40 -0700

    Dropping stats from the reports page.

--- plugins/maker-faire-forms/maker-faire-forms.php
+++ plugins/maker-faire-forms/maker-faire-forms.php
@@ -2611,7 +2611,7 @@ class MAKER_FAIRE_FORM {
 			}
 		}
 	
-		$stats = $this->get_reports_stats();
+		// $stats = $this->get_reports_stats();
 		$fails = $this->get_failed_syncs();
 		
 		?>
@@ -2619,7 +2619,7 @@ class MAKER_FAIRE_FORM {
 			<?php echo screen_icon(); ?>
 			<h2>Maker Faire Reports</h2>
 			<div style="width:45%; float:left">
-				<h1>Stats</h1>
+				<!-- <h1>Stats</h1>
 				<table width="100%" border="0" cellspacing="0" cellpadding="3" style="border:1px solid #DFDFDF;">
 					<thead>
 						<tr>
@@ -2630,16 +2630,16 @@ class MAKER_FAIRE_FORM {
 						</tr>
 					</thead>
 					<tbody>
-						<?php foreach( $stats as $status => $details ) : ?>
+						<?php // foreach( $stats as $status => $details ) : ?>
 						<tr>
-							<td><?php echo esc_html( ucwords( str_replace( '-', ' ', $status ) ) ); ?></td>
-							<?php foreach( $details as $mftype => $num ) : ?>
-							<td><?php echo intval( $num ); ?></td>
-							<?php endforeach; ?>
+							<td><?php // echo esc_html( ucwords( str_replace( '-', ' ', $status ) ) ); ?></td>
+							<?php // foreach( $details as $mftype => $num ) : ?>
+							<td><?php // echo intval( $num ); ?></td>
+							<?php // endforeach; ?>
 						</tr>
-						<?php endforeach; ?>
+						<?php // endforeach; ?>
 					</tbody>
-			</table>
+				</table>-->
 			<h1 style="margin-top:40px;">Sync Status with JDB</h1>
 			Syncs 100 applications at a time.<br />To do a full sync start at 0 and increase by 100 until you're done.
 			<form action="" method="post">
