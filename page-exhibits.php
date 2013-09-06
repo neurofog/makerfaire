f469ae19ba6797a69429e52cb278139bada0cde6
Cole Geissinger <cgeissinger@makermedia.com>
2013-09-06 11:12:13 -0700

    Fixing search for the makers listing pages

--- page-exhibits.php
+++ page-exhibits.php
@@ -18,7 +18,12 @@ get_header(); ?>
 				
 			</div>
 			
-			<?php get_search_form(); ?>
+			<form role="search" method="get" class="form-search" id="searchform" action="<?php echo home_url( '/' ); ?>">
+				<input type="text" value="<?php echo get_search_query( true ); ?>" name="s" id="s" class="input-medium search-query" />
+				<input type="hidden" name="post_type" value="mf_form" />
+				<input type="hidden" name="faire" value="maker-faire-bay-area-2013" />
+				<input type="submit" id="searchsubmit" class="btn btn-primary" value="Search" />
+			</form>
 			
 			<?php
 			$paged = get_query_var('paged') ? get_query_var('paged') : 1;
