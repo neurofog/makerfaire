98e26f9cc2994fbd7bc34aeb5cccdf09e6daa9cb
Cole Geissinger <cgeissinger@makermedia.com>
2013-09-11 15:15:59 -0700

    Adding a schedule new event button to applications. Fixing display issue with the locations metabox

--- js/expand-post-edit.js
+++ js/expand-post-edit.js
@@ -18,10 +18,11 @@ jQuery( document ).ready( function( $ ) {
 	$( '#locationchecklist > li > label' ).each( function() {
 		// Get the current text
 		var location_name = $(this).text();
-
+		var selected = $(this).find('input').attr('checked');
 		// Check if the string does NOT start with a_ (our unique identifiyer for WMFNY13)
 		// In the future we will be removing this and finding a better solution.
-		if ( location_name.indexOf('a_') === -1 ) {
+		console.log(selected + ' - ' + typeof selected);
+		if ( location_name.indexOf('a_') === -1 && selected !== 'checked' ) {
 			$(this).parent().hide();
 		}
 	});
