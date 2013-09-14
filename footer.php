20fe1d248dbb72584dc96d0609ab3d0fff4afe16
Cole Geissinger <cgeissinger@makermedia.com>
2013-09-13 22:13:59 -0700

    Adding Crazy Egg tracking code

--- footer.php
+++ footer.php
@@ -170,6 +170,14 @@ jQuery(document).ready(function(){
 });
 </script>
 
+<?php // Adding Crazy Egg tracking ?>
+<script type="text/javascript">
+setTimeout(function(){var a=document.createElement("script");
+var b=document.getElementsByTagName("script")[0];
+a.src=document.location.protocol+"//dnn506yrbagrg.cloudfront.net/pages/scripts/0013/2533.js?"+Math.floor(new Date().getTime()/3600000);
+a.async=true;a.type="text/javascript";b.parentNode.insertBefore(a,b)}, 1);
+</script>
+
 <?php wp_footer(); ?>
 
 </body>
