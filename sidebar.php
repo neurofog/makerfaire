60964a9f85db84d64895468ff930b963c8a1951b
Cole Geissinger <cgeissinger@makermedia.com>
2013-09-22 15:34:51 -0400

    updating the sidebar for phase 3 of Maker Faire

--- sidebar.php
+++ sidebar.php
@@ -3,18 +3,8 @@
 	<div class="sidebar-bordered">
 
 		<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/mr-makey.png" alt="Mr. Makey" class="makey pull-left">
-		<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/countdown.png" alt="New York Maker Faire" class="counter pull-left"
 
-		<p class="buy-tickets"><a href="http://makerfairenyc.eventbrite.com/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/buy-tickets.png" alt="Buy tickets for Maker Faire" class=""></a></p>
-
-		<h5 class="count-down-sub">New York Hall of Science</h4>
-		<h5 class="count-down-sub-sub">Sat. 10 AM - 7 PM | Sun. 10 AM - 6 PM</h5>
-
-	</div>
-
-	<div class="sidebar-bordered">
-
-		<h3>Get news and updates on all faires and calls for makers.</h3>
+		<h3>Get news<br />and updates<br /> on all faires<br /> and calls to makers.</h3>
 
 		<form class="form-inline" action="http://makermedia.createsend.com/t/r/s/jjuruj/" method="post" id="subForm"> 
 			<input type="text" placeholder="Enter your email" class="news-signup" name="cm-jjuruj-jjuruj" id="jjuruj-jjuruj">
@@ -54,50 +44,7 @@
 		</div>
 
 	</div>
-
-	<div class="center twitter">
-		<a href="https://twitter.com/makerfaire" class="twitter-follow-button" data-show-count="false" data-size="large">Follow @makerfaire</a>
-		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
-		<a class="twitter-timeline" href="https://twitter.com/search?q=%23makerfaire" data-widget-id="322225978648698880">Tweets about "#makerfaire"</a>
-		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-	</div>
-
-	<div class="sidebar-bordered sponsored">
-
-		<h3>Presented By</h3>
-
-		<div class="center">
-			<a href="http://disney.com/">
-			<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sponsors/disney.png" alt="Disney" class="center"></a>
-		</div>
-
-		<h3>Goldsmith Sponsors</h3>
-
-		<div id="myCarousel" class="carousel slide">
-			<div class="carousel-inner">
-				<?php echo mf_sponsor_carousel( 'Goldsmith Sponsor' ); ?>
-			</div>
-		</div>
-
-		<h3>Silversmith Sponsors</h3>
-
-		<div id="myCarousel" class="carousel slide">
-			<div class="carousel-inner">
-				<?php echo mf_sponsor_carousel( 'Silversmith Sponsor' ); ?>
-			</div>
-		</div>
-
-		<h3>Coppersmith Sponsors</h3>
-
-		<div id="myCarousel" class="carousel slide">
-			<div class="carousel-inner">
-				<?php echo mf_sponsor_carousel( 'Coppersmith Sponsor' ); ?>
-			</div>
-		
-		</div>
-
-	</div>
-
+	
 	<!-- Beginning Sync AdSlot 2 for Ad unit header ### size: [[300,250]]  -->
 	<div id='div-gpt-ad-664089004995786621-2' class="adblock">
 		<script type='text/javascript'>
@@ -105,6 +52,13 @@
 		</script>
 	</div>
 	<!-- End AdSlot 2 -->
+
+	<div class="center twitter">
+		<a href="https://twitter.com/makerfaire" class="twitter-follow-button" data-show-count="false" data-size="large">Follow @makerfaire</a>
+		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+		<a class="twitter-timeline" href="https://twitter.com/search?q=%23makerfaire" data-widget-id="322225978648698880">Tweets about "#makerfaire"</a>
+		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+	</div>
 
 	<!-- Beginning Sync AdSlot 3 for Ad unit header ### size: [[300,250]]  -->
 	<div id='div-gpt-ad-664089004995786621-3' class="adblock">
