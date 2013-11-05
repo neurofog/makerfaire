de8a8d65a53ce0f39fe480b817d19abc0516ea40
Cole Geissinger <cgeissinger@makermedia.com>
2013-11-05 14:16:17 -0800

    Beginnings of the ajax request to sign in a user

--- plugins/gigya/gigya.php
+++ plugins/gigya/gigya.php
@@ -34,10 +34,13 @@
 
 		public function __construct() {
 			// Load our GSSDK
-			add_action( 'wp_head', array( $this, 'make_gigya_socialize_api' ), 999 );
+			add_action( 'wp_head', array( $this, 'socialize_api' ), 999 );
 
 			// Load our resources
-			add_action( 'wp_enqueue_scripts', array( $this, 'make_gigya_resources' ), 30 );
+			add_action( 'wp_enqueue_scripts', array( $this, 'load_resources' ), 30 );
+
+			// Process our ajax requests
+			 add_action( 'wp_ajax_nopriv_ajaxlogin', array( $this, 'ajax_login' ) );
 		}
 
 		/**
@@ -47,7 +50,7 @@
 		 * @return html
 		 * @since  SPRINT_NAME
 		 */
-		public function make_gigya_socialize_api() { ?>
+		public function socialize_api() { ?>
 			<script src="http://cdn.gigya.com/JS/socialize.js?apikey=3_bjlVxC0gadZ6dN9q8K1ePCbDHtATT8OgJxZJcsr0ty8o5oKnvjI_G2DOZ1YasJHF">{ enabledProviders: '*' }</script>
 		<?php }
 		
@@ -59,8 +62,27 @@
 		 *
 		 * @since  SPRINT_NAME
 		 */
-		public function make_gigya_resources() {
+		public function load_resources() {
 			wp_enqueue_script( 'make_socialize', MAKE_GIGYA_URL . '/js/socialize.js', array( 'jquery' ), '0.1', true );
+			wp_localize_script( 'make_socialize', 'make_gigya', array(
+				'ajax' => admin_url( 'admin-ajax.php' ),
+				'loading' => 'Loading',
+				'secure_it' => wp_create_nonce( 'ajax-login-nonce' ),
+			) );
+		}
+
+
+		public function ajax_login() {
+			var_dump($_POST);
+			// // Check our nonce and make sure it's correct
+			// check_ajax_referer( 'ajax-login-nonce', 'nonce' );
+
+			// // Hold onto the submission data
+			// $data = array();
+
+			// if ( isset( $_POST['request'] ) && $_POST['request'] == 'login' ) {
+			// 	var_dump($_POST);
+			// }
 		}
 
 	}
--- plugins/gigya/js/socialize.js
+++ plugins/gigya/js/socialize.js
@@ -4,7 +4,39 @@
  * @since  SPRINT_NAME
  */
 
-gigya.socialize.addEventHandlers({ // http://developers.gigya.com/020_Client_API/010_Socialize/socialize.addEventHandlers
+// Set debugging mode.
+var gigya_debug = true;
+
+
+/**
+ * Displays the login plugin
+ *
+ * http://developers.gigya.com/020_Client_API/010_Socialize/Socialize.showLoginUI
+ * 
+ * @since SPRINT_NAME
+ */
+gigya.socialize.showLoginUI({
+	height: 100,
+	width: 330,
+	showTermsLink: false,
+	hideGigyaLink: true,
+	buttonsStyle: 'fullLogo', // Change the default buttons design to "Full Logos" design
+	showWhatsThis: true, // Pop-up a hint describing the Login Plugin, when the user rolls over the Gigya link.
+	containerID: 'modal-body', // The component will embed itself inside the loginDiv Div
+	cid: ''
+});
+
+
+/**
+ * The Gigya service generates several global application events for various situations that are driven by user interactions.
+ * Global application events are fired whenever the event to which they refer occurs, regardless of what was the action that triggered the event.
+ * This is in contrast to plugin events, which are only fired by the specific plugin on which they were configured.
+ * This method allows setting event handlers for each of the supported global events.
+ * NOTE: http://developers.gigya.com/020_Client_API/010_Socialize/socialize.addEventHandlers
+ *
+ * @since  SPRINT_NAME
+ */
+gigya.socialize.addEventHandlers({ // 
 	onLogin: on_login,
 	onLogout: on_logout
 });
@@ -21,42 +53,70 @@ gigya.socialize.addEventHandlers({ // http://developers.gigya.com/020_Client_API
  */
 function on_login( eventObj ) {
 	// Results: "congrats on your login to {PROVIDER}! {provider} user ID: {ProviderUID}"
-    console.log( 'Logged in to ' + eventObj.provider + '!\nUser ID: ' +  eventObj.user.identities[ eventObj.provider ].providerUID );
+	if ( gigya_debug )
+		console.log( 'Logged in to ' + eventObj.provider + '!\nUser ID: ' +  eventObj.user.identities[ eventObj.provider ].providerUID );
 
     // Verify the signature ...
     verify_signature( eventObj.UID, eventObj.signatureTimestamp, eventObj.UIDSignature );
+
+    // Send our data via Ajax to the server to verify if the user is a returning user or a new one
+    jQuery.ajax({
+		type: 'POST',
+		dataType: 'json',
+		url: make_gigya.ajax,
+		data: {
+			request: 'login',
+			user: eventObj.user,
+			nonce: make_gigya.secure_it
+		},
+		success: function( results ) {
+			if ( results.loggedin === true ) {
+				if ( gigya_debug )
+					console.log( 'WordPress logged in' );
+			} else {
+				if ( gigya_debug )
+					console.log( 'Failed WordPress login' );
+			}
+		},
+		complete: function( jqXHR, textStatus ) {
+			console.log( 'AJAX complete' );
+		}
+    });
+
 
     // Check whether the user is new by searching if eventObj.UID exists in your database (link this to maker profiles)
     var newUser = true; // Let's assume the user is new
     
     if ( newUser ) {
-        // 1.  Register user
-        // 2.  Store new user in DB
-        // 3.  link site account to social network identity
-        // 3.1 first construct the linkAccounts parameters
         var siteUID = 'uTtCGqDTEtcZMGL08w'; // The user ID that you have designated to the current user on your user management system. This site UID must be different than the Gigya ID.
         var dateStr = Math.round( new Date().getTime() / 1000.0 ); // Current time in Unix format
         var yourSig = encodeURIComponent( siteUID, dateStr );
         var params = {
-            siteUID: siteUID, 
+            siteUID: siteUID,
             timestamp: dateStr,
 			cid: '',
             signature: yourSig
         };
         
-        // 3.1 call linkAccounts method:
         // http://developers.gigya.com/020_Client_API/010_Socialize/socialize.notifyRegistration  
         gigya.socialize.notifyRegistration( params ); // Use the REST API http://developers.gigya.com/037_API_reference/010_Socialize/socialize.notifyRegistration
     }
 	
 	// Update our links and remove any of the modal stuffffffsssss.
-	jQuery( '.user-creds' ).addClass( 'logged-in' ).find( 'a' ).removeAttr( 'data-toggle' ).text( 'Logout' );
+	jQuery( '.user-creds' ).addClass( 'logged-in' ).find( 'a' ).attr( 'href', '#logout' ).removeAttr( 'data-toggle' ).text( 'Logout' );
 
 	// Close the modal when we have logged in.
 	jQuery( '#login' ).modal( 'hide' );
 
-	$( '.user-creds.logged-in' ).click( function() {
-		console.log('CLICKED');
+	if ( gigya_debug )
+		console.log('LOGIN DONE');
+
+	// Let's hook into the new content update and allow users to sign out
+	jQuery( '.user-creds.logged-in a[href="#logout"]' ).click( function(e) {
+		if ( gigya_debug )
+			console.log('CLICKED');
+
+		e.preventDefault();
 		logout_gigya();
 	});
 
@@ -64,9 +124,9 @@ function on_login( eventObj ) {
 
 /**
  * Creates an encoded URI to pass to notifyRegistration
- * @param  string   UID       [description]
- * @param  {[type]} timestamp [description]
- * @return {[type]}           [description]
+ * @param  string  UID       The User ID that should be used for login verification
+ * @param  integer timestamp The GMT time of the response in UNIX time format. The time stamp should be used for login verification
+ * 
  * @since  SPRINT_NAME
  */
 function createSignature( UID, timestamp ) {
@@ -87,15 +147,18 @@ function createSignature( UID, timestamp ) {
 function verify_signature( UID, timestamp, signature ) {
 	encodedUID = encodeURIComponent( UID ) ; // encode the UID parameter before sending it to the server. On server side use decodeURIComponent() function to decode an encoded UID
 
-    console.log( 'UID: ' + UID + '\nTimestamp: ' + timestamp + '\nSignature: ' + signature + '\nYour UID encoded: ' + encodedUID );
+	if ( gigya_debug )
+		console.log( 'UID: ' + UID + '\nTimestamp: ' + timestamp + '\nSignature: ' + signature + '\nYour UID encoded: ' + encodedUID );
 }
 
 /**
  * Logout from Gigya platform. This method is activated when "Logout" button is clicked
+ * NOTE: http://developers.gigya.com/020_Client_API/010_Socialize/socialize.logout
+ * 
  * @since SPRINT_NAME
  */
 function logout_gigya() {
-    gigya.services.socialize.logout(); // logout from Gigya platform
+    gigya.services.socialize.logout();
 }
 
 /**
@@ -104,31 +167,14 @@ function logout_gigya() {
  * @param  {[type]} eventObj [description]
  * @since SPRINT_NAME
  */
-function on_logout(eventObj) {
+function on_logout( eventObj ) {
 	alert( 'You have been successfully logged out!' );
+
+	jQuery( '.user-creds.logged-in' ).removeClass( 'logged-in' ).find( 'a' ).attr({
+		'href' : '#login',
+		'data-toggle': 'modal'
+	}).text( 'Login/Register' );
+
+	if ( gigya_debug )
+		console.log( 'User logged out' );
 }
-
-/**
- * Displays the login plugin
- *
- * http://developers.gigya.com/020_Client_API/010_Socialize/Socialize.showLoginUI
- * 
- * @since SPRINT_NAME
- */
-gigya.socialize.showLoginUI({
-	height: 100,
-	width: 330,
-	showTermsLink: false,
-	hideGigyaLink: true,
-	buttonsStyle: 'fullLogo', // Change the default buttons design to "Full Logos" design
-	showWhatsThis: true, // Pop-up a hint describing the Login Plugin, when the user rolls over the Gigya link.
-	containerID: 'modal-body', // The component will embed itself inside the loginDiv Div
-	cid: ''
-});
-
-jQuery( 'document' ).ready( function( $ ) {
-	$( '.user-creds.logged-in' ).click( function() {
-		console.log('CLICKED');
-		logout_gigya();
-	});
-});
