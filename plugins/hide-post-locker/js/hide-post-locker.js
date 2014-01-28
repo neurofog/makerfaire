a9baef60fe9c0a957d3d1572785d1e4d6cadb421
Cole Geissinger <cgeissinger@makermedia.com>
2014-01-28 15:40:26 -0800

    Updating the “view application” option when multiple users want to review an application

---
+++ plugins/hide-post-locker/js/hide-post-locker.js
@@ -0,0 +1,29 @@
+jQuery(document).ready(function( $ ) {
+	
+	// Make sure our button actually exists...
+	if ( $( '#post-lock-dialog' ).length === 1 ) {
+
+		// Listen for a click to hide the post locker
+		$( '.hide-post-locker-btn' ).click( function() {
+
+			// Disable any actions a user can take...
+			$( '#delete-action .submitdelete, #publishing-action #publish' ).attr( 'disabled', 'disabled' );
+
+			// Remove the post lock dialog. Thanks to the Heartbeat API, we can't just hide it.
+			// TODO: fix this up so we can reinvoke the post locker without a page refresh
+			$( '#post-lock-dialog' ).remove();
+
+			// Display our Preview message
+			$( '.cg-hpl-preview-wrapper' ).fadeIn();
+
+		});
+
+
+		// Listen for a click on our show post locker link
+		$( '.show-post-locker-btn' ).click( function() {
+
+			// For now, we'll force a page refresh but a proper way to call the post locker would be preferred.
+			location.reload();
+		});
+	}
+});
