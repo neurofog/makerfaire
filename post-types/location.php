7951b4377e6831b573f15f6e872e52079e5fb6a2
Jake Spurlock <jspurlock@makermedia.com>
2014-02-11 10:39:02 -0800

    Boom, basic implementation.

--- post-types/location.php
+++ post-types/location.php
@@ -1,15 +1,16 @@
 <?php
 
-function location_init() {
+function location_post_type_init() {
 	register_post_type( 'location', array(
-		'hierarchical'      => false,
+		'hierarchical'      => true,
 		'public'            => true,
 		'show_in_nav_menus' => true,
 		'show_ui'           => true,
-		'supports'          => array( 'title', 'editor' ),
+		'supports'          => array( 'title', 'editor', 'page-attributes', 'revisions', 'excerpt' ),
 		'has_archive'       => true,
 		'query_var'         => true,
 		'rewrite'           => true,
+		'taxonomies'		=> array( 'faire' ),
 		'labels'            => array(
 			'name'                => __( 'Locations', 'makerfaire' ),
 			'singular_name'       => __( 'Locations', 'makerfaire' ),
@@ -26,9 +27,9 @@ function location_init() {
 			'menu_name'           => __( 'Locations', 'makerfaire' ),
 		),
 	) );
-
 }
-add_action( 'init', 'location_init' );
+
+add_action( 'init', 'location_post_type_init' );
 
 function location_updated_messages( $messages ) {
 	global $post;
@@ -55,3 +56,130 @@ function location_updated_messages( $messages ) {
 	return $messages;
 }
 add_filter( 'post_updated_messages', 'location_updated_messages' );
+
+
+/**
+ * Adds a box to the main column on the Post and Page edit screens.
+ */
+function mf_add_custom_box() {
+
+    $screens = array( 'mf_form', 'event-items' );
+
+    foreach ( $screens as $screen ) {
+
+        add_meta_box(
+            'mf_sectionid',
+            __( 'Location', 'makerfaire' ),
+            'mf_inner_location_box',
+            $screen
+        );
+    }
+}
+add_action( 'add_meta_boxes', 'mf_add_custom_box' );
+
+/**
+ * Prints the box content.
+ * 
+ * @param WP_Post $post The object for the current post/page.
+ */
+function mf_inner_location_box( $post ) {
+
+	// Add an nonce field so we can check for it later.
+	wp_nonce_field( 'mf_inner_location_box', 'mf_inner_location_box_nonce' );
+
+	$faire_location = get_post_meta( $post->ID, 'faire_location', true );
+
+	// $faire_location = array( 101, 102 );
+
+	var_dump( $faire_location );
+
+	$faires = get_the_terms( $post, 'faire' );
+
+	$faire = array();
+
+	foreach ($faires as $da_faire ) {
+		$faire[] = $da_faire->slug;
+	}
+
+  	// WP_Query arguments
+	$args = array (
+		'post_type'	=> 'location',
+		'tax_query' => array(
+			array(
+				'taxonomy' 	=> 'faire',
+				'field' 	=> 'slug',
+				'terms' 	=> $faire,
+			)
+		)
+	);
+
+	// The Query
+	$query = new WP_Query( $args );
+	echo '<ul>';
+	foreach ( $query->posts as $location ) {
+		echo '<li><label class="checkbox">';
+		if ( in_array( $location->ID, $faire_location ) ) {
+			echo '<input type="checkbox" name="location[]" value="' . esc_attr( $location->ID ) .'" checked>';	
+		} else {
+			echo '<input type="checkbox" name="location[]" value="' . esc_attr( $location->ID ) .'">';
+		}
+		echo wp_kses_post( $location->post_title );
+		echo '</label"></li>';
+
+	}
+	echo '</ul>';
+
+}
+
+/**
+ * When the post is saved, saves our location data.
+ *
+ * @param int $post_id The ID of the post being saved.
+ */
+function mf_save_postdata( $post_id ) {
+
+  // var_dump( $_POST );
+
+  /*
+   * We need to verify this came from the our screen and with proper authorization,
+   * because save_post can be triggered at other times.
+   */
+
+  // Check if our nonce is set.
+  if ( ! isset( $_POST['mf_inner_location_box_nonce'] ) )
+    return $post_id;
+
+  $nonce = $_POST['mf_inner_location_box_nonce'];
+
+  // Verify that the nonce is valid.
+  if ( ! wp_verify_nonce( $nonce, 'mf_inner_location_box' ) )
+      return $post_id;
+
+  // If this is an autosave, our form has not been submitted, so we don't want to do anything.
+  if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) 
+      return $post_id;
+
+  // Check the user's permissions.
+  if ( 'page' == $_POST['post_type'] ) {
+
+    if ( ! current_user_can( 'edit_page', $post_id ) )
+        return $post_id;
+  
+  } else {
+
+    if ( ! current_user_can( 'edit_post', $post_id ) )
+        return $post_id;
+  }
+
+  /* OK, its safe for us to save the data now. */
+
+  // Sanitize user input, and save to post meta.
+
+  $locations = array();
+  foreach ( $_POST['location'] as $location ) {
+  	$locations[] = $location;
+  }
+  update_post_meta( $post_id, 'faire_location', $locations );
+
+}
+add_action( 'save_post', 'mf_save_postdata' );
