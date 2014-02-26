a89568d3317317fa41ccac9d9f3f4ef2b8d46c62
Cole Geissinger <cgeissinger@makermedia.com>
2014-02-26 13:18:43 -0800

    Fixing the top blue banner

--- header.php
+++ header.php
@@ -67,7 +67,7 @@ Topbar
 ======
 -->
 
-<div class="navbar navbar-fixed-top">
+<div class="navbar">
 	<div class="navbar-inner">
 		<div class="container">
 		<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
