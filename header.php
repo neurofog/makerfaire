33930c3e6fa0606e9f230318b18ebfe9da5e5339
David Beauchamp <dbeauchamp@makermedia.com>
2014-02-26 17:01:42 -0800

    Adding a Google Tag Manager script for Shed  ads.

--- header.php
+++ header.php
@@ -56,11 +56,23 @@
 		</script>	
 	<?php endif; ?>
 
+	<script type="text/javascript">
+		dataLayer = [];
+	</script>
 	</head>
 
 <body id="bootstrap-js" <?php body_class('no-js'); ?>>
+	<!-- Google Tag Manager -->
+	<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-WR8NLB"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-WR8NLB');</script>
+	<!-- End Google Tag Manager -->
 
-<script type="text/javascript">document.body.className = document.body.className.replace('no-js','js');</script> 
+	<script type="text/javascript">document.body.className = document.body.className.replace('no-js','js');</script> 
 <!-- 
 ======
 Topbar
