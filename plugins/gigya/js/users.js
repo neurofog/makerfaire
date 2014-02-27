1b0b270af4f4f203f88b6ff6436e2b0ad4c2a74e
Cole Geissinger <cgeissinger@makermedia.com>
2014-02-26 20:31:12 -0800

    Fixing the forms to be editable when they are in-progress after the call has closed

--- plugins/gigya/js/users.js
+++ plugins/gigya/js/users.js
@@ -66,7 +66,7 @@ function makerfaire_profile( maker ) {
 					// Check if the date right now is after our end date, 'August 4th, 2013', or else close the forms
 					if ( Date.parse( app_date ) >= Date.parse( expire_date ) ) {
 						// if the current time is less than the end date, we'll handle this form with the ability to still edit.
-						if ( type === 'presenter' || ( now.getTime() < end_date.getTime() && ( results.forms[ type ][ app_id ]['post_status'] === 'in-progress' || results.forms[ type ][ app_id ]['post_status'] === 'proposed' ) ) ) {
+						if ( ( results.forms[ type ][ app_id ]['post_status'] === 'in-progress' || type === 'presenter' ) || now.getTime() < end_date.getTime() ) {
 							append = '<li><a href="' + make_gigya.root_path + type + 'form/?id=' + app_id + '">' +  app_id + ' - ' + results.forms[ type ][ app_id ]['post_title'] + ' (' + results.forms[ type ][ app_id ]['post_status'] + ')</a></li>';
 						} else {
 							append = '<li>' + app_id + ' - ' + results.forms[ type ][ app_id ]['post_title'] + ' (' + results.forms[ type ][ app_id ]['post_status'] + ')</li>';
