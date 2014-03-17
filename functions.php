4e8802f333f019510ca2687480c9b08dd9e9c6b9
Cole Geissinger <colegeissinger@gmail.com>
2014-03-17 14:08:22 -0700

    Adding some more characters to the cleaning process

--- functions.php
+++ functions.php
@@ -97,8 +97,8 @@ add_action( 'wp_enqueue_scripts', 'make_enqueue_jquery' );
  * @since L-Ron
  */
 function mf_clean_content( $content, $report = false ) {
-	$bad = array( '&#039;', "\'", '\"', '&#8217;', 'u2019', '&#38;', '&#038;', '&amp;', '&#34;', '&#034;', 'u201c', 'u201d', '&#8211;', '&lt;', '&#8230;', 'u00a0', 'u2013', '00ae', 'u016f', 'u0161', 'u00e1' );
-	$good = array( "'",     "'",  '"',  "'",       "'",     '&',     '&',      '&',     '"',     '"',      '"',     '"',     '–',       '>',    '...',     ' ',     '-',     '®',    'ů',     'š',     'á' );
+	$bad = array( '&#039;', "\'", '\"', '&#8217;', 'u2019', '&#38;', '&#038;', '&amp;', '&#34;', '&#034;', 'u201c', 'u201d', '&#8211;', '&lt;', '&#8230;', 'u00a0', 'u2013', '00ae', 'u016f', 'u0161', 'u00e1', 'u2122', 'u00a9' );
+	$good = array( "'",     "'",  '"',  "'",       "'",     '&',     '&',      '&',     '"',     '"',      '"',     '"',     '–',       '>',    '...',     ' ',     '-',     '®',    'ů',     'š',     'á',		'™',	 '©' );
 
 	// If we are are correct HTML, let's add that here as reports we don't want HTML shown in here
 	if ( $report ) {
