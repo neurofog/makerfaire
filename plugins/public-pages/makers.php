f45a118d8ada2106fbffc5a1a4e3a8f20278d5d1
Jake Spurlock <jspurlock@makermedia.com>
2014-04-29 07:40:49 -0700

    Use the actual user photo, not just check for it.

--- plugins/public-pages/makers.php
+++ plugins/public-pages/makers.php
@@ -1089,8 +1089,8 @@ function mf_display_schedule_by_location( $atts ) {
 									// We may want to over ride the photo of an application on the schedule page by checking if there is a featured image on the event item
 									if ( has_post_thumbnail( absint( $event_id ) ) ) {
 										$output .= get_the_post_thumbnail( absint( $event_id ), 'schedule-thumb' );
-									} elseif ( ! empty( $app->{ $mfform->merge_fields( 'user_photo', $app->form_type ) } ) ) {
-										$output .= '<a href="' . get_permalink( absint( $app_obj->ID ) ) . '"><img src="' . wpcom_vip_get_resized_remote_image_url( mf_get_the_maker_image( $app ), 140, 140 ) . '" alt="' . esc_attr( $app_obj->post_title ) . '" width="140" height="140"></a>';
+									} elseif ( ! empty( $app->{ $mfform->merge_fields( 'user_photo', $app->form_type ) }[0] ) ) {
+										$output .= '<a href="' . get_permalink( absint( $app_obj->ID ) ) . '"><img src="' . wpcom_vip_get_resized_remote_image_url( $app->{ $mfform->merge_fields( 'user_photo', $app->form_type ) }[0], 140, 140 ) . '" alt="' . esc_attr( $app_obj->post_title ) . '" width="140" height="140"></a>';
 									}
 									else {
 										$output .= '<a href="' . get_permalink( absint( $app_obj->ID ) ) . '"><img src="' . wpcom_vip_get_resized_remote_image_url( mf_get_the_maker_image( $app ), 140, 140 ) . '" alt="' . esc_attr( $app_obj->post_title ) . '" width="140" height="140"></a>';
