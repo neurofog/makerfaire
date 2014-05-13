de394d3548904939a853004770c2cc02b2fac1df
Jake Spurlock <jspurlock@makermedia.com>
2014-05-12 23:57:13 -0700

    Javascript to handle the post deletion.

--- plugins/maker-faire-forms/assets/js/reports.js
+++ plugins/maker-faire-forms/assets/js/reports.js
@@ -1,5 +1,28 @@
 jQuery(document).ready(function($) {
 
+	$( '.deleteme' ).on( 'click', function( e ) {
+
+		// Prevent the button from triggering
+		e.preventDefault();
+
+		var meta = $(this).data();
+
+		meta.action = 'mf_delete_scheduled_event';
+
+		// Make the ajax request with the form data.
+		$.ajax({
+			url: ajaxurl,
+			data: meta,
+			type: 'POST',
+			success: function( data ){
+				blurb = JSON.parse( data );
+				if ( blurb.pid ) {
+					$('.post-' + blurb.pid ).slideUp();
+				}
+			}
+		});
+	});
+
 	console.log('loaded');
 	// Listen for a click on the editoral button
 	$('input[type="submit"].export_comments').click(function(e) {
