47d8f302c4030250593ba988d22b91eddec97ab4
Jake Spurlock <jspurlock@makermedia.com>
2014-05-17 13:35:30 -0700

    Drop the countdown.

--- sidebar.php
+++ sidebar.php
@@ -9,17 +9,17 @@
 
                         <script type="text/javascript">
 
-                                jQuery(document).ready(function() {
-                                        mfba = new Date(2014, 5-1, 17, 9, 00);
-                                        jQuery('.countdown').countdown({
-                                                until: mfba,
-                                                timezone: -8,
-                                                format: 'DHMS',
-                                                layout:'<div class="countdown-numbers"><table><tr><th>{dnn}</th><th>{sep}</td><th>{hnn}</th><th>{sep}</td><th>{mnn}</th><th>{sep}</td><th>{snn}</th></tr><tr class="time"><td>Days</td><td></td><td>Hours</td><td></td><td>Minutes</td><td></td><td>Seconds</td></tr></table></div>',
-                                                timeSeparator:'<span class="separator">:</span>',
-                                        });
+                                // jQuery(document).ready(function() {
+                                //         mfba = new Date(2014, 5-1, 17, 9, 00);
+                                //         jQuery('.countdown').countdown({
+                                //                 until: mfba,
+                                //                 timezone: -8,
+                                //                 format: 'DHMS',
+                                //                 layout:'<div class="countdown-numbers"><table><tr><th>{dnn}</th><th>{sep}</td><th>{hnn}</th><th>{sep}</td><th>{mnn}</th><th>{sep}</td><th>{snn}</th></tr><tr class="time"><td>Days</td><td></td><td>Hours</td><td></td><td>Minutes</td><td></td><td>Seconds</td></tr></table></div>',
+                                //                 timeSeparator:'<span class="separator">:</span>',
+                                //         });
 
-                                });
+                                // });
                         </script>
 
                 </div>
