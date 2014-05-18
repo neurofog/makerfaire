01492f231154c1d97ae33309cd18b29b221b803a
Jake Spurlock <jspurlock@makermedia.com>
2014-05-18 12:31:42 -0700

    Moved things around.

--- sidebar.php
+++ sidebar.php
@@ -5,35 +5,7 @@
                 <img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/images/mr-makey.png" alt="Mr. Makey" class="makey pull-left">
                 <h3 class="counter-title">Maker Faire Bay Area<br />May 17 &amp; 18, 2014</h3>
 
-                <div class="countdown">
-
-                        <script type="text/javascript">
-
-                                // jQuery(document).ready(function() {
-                                //         mfba = new Date(2014, 5-1, 17, 9, 00);
-                                //         jQuery('.countdown').countdown({
-                                //                 until: mfba,
-                                //                 timezone: -8,
-                                //                 format: 'DHMS',
-                                //                 layout:'<div class="countdown-numbers"><table><tr><th>{dnn}</th><th>{sep}</td><th>{hnn}</th><th>{sep}</td><th>{mnn}</th><th>{sep}</td><th>{snn}</th></tr><tr class="time"><td>Days</td><td></td><td>Hours</td><td></td><td>Minutes</td><td></td><td>Seconds</td></tr></table></div>',
-                                //                 timeSeparator:'<span class="separator">:</span>',
-                                //         });
-
-                                // });
-                        </script>
-
-                </div>
-
-                <p class="buy-tickets"><a href="http://www.eventbrite.com/e/maker-faire-bay-area-2014-tickets-9098302267?aff=MFwbBuytix" target="_blank"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/images/buy-tickets.png" alt="Buy tickets for Maker Faire San Mateo"></a></p>
-
-                <h5 class="count-down-sub">May 17 &amp; 18<br />San Mateo Event Center</h5>
-                <h5 class="count-down-sub-sub">Sat. 10 AM - 8 PM | Sun. 10 AM - 6 PM</h5>
-
-        </div>
-
-        <div class="sidebar-bordered-nl">
-
-                <h3>Get news and updates on all faires and calls for makers.</h3>
+                <h3>Get news and updates on all <br>faires and calls for makers.</h3>
 
                 <form class="form-inline" action="http://makermedia.createsend.com/t/r/s/jjuruj/" method="post" id="subForm">
                         <input type="text" placeholder="Enter your email" class="news-signup" name="cm-jjuruj-jjuruj" id="jjuruj-jjuruj">
@@ -73,6 +45,13 @@
 
         </div>
 
+        <div class="center twitter">
+          <a href="https://twitter.com/makerfaire" class="twitter-follow-button" data-show-count="false" data-size="large">Follow @makerfaire</a>
+          <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+          <a class="twitter-timeline" href="https://twitter.com/search?q=%23makerfaire" data-widget-id="322225978648698880">Tweets about "#makerfaire"</a>
+          <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+        </div>
+
         <div class="sidebar-bordered sponsored">
 
                <h3><a href="<?php echo esc_url( home_url( '/sponsors' ) ); ?>">Goldsmith Sponsors</a></h3>
@@ -109,13 +88,6 @@
 		</script>
 	</div>
 	<!-- End AdSlot 2 -->
-
-	<div class="center twitter">
-		<a href="https://twitter.com/makerfaire" class="twitter-follow-button" data-show-count="false" data-size="large">Follow @makerfaire</a>
-		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
-		<a class="twitter-timeline" href="https://twitter.com/search?q=%23makerfaire" data-widget-id="322225978648698880">Tweets about "#makerfaire"</a>
-		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-	</div>
 
 	<!-- Beginning Sync AdSlot 3 for Ad unit header ### size: [[300,250]]  -->
 	<div id='div-gpt-ad-664089004995786621-3' class="adblock">
