fcf706199754fa1eb7b3a478ea52a6acfcbd81a3
Jake Spurlock <whyisjake@gmail.com>
2014-06-18 11:44:41 -0700

    Fix for the Javascript

--- plugins/white-house/white-house.js
+++ plugins/white-house/white-house.js
@@ -8,10 +8,10 @@ jQuery( document ).ready( function( $ ) {
 		output = '<div class="row">';
 		$.each( $( the_posts ), function( i, post ){
 			output += '<div class="span3">';
-			output += '<a href="' + post.permalink  + '">;
+			output += '<a href="' + post.URL  + '">';
 			output += '<img class="thumbnail" src="' + post.featured_image + '?w=220&h=160&crop=1">';
 			output += '</a>;
-			output += '<a href="' + post.permalink  + '">;
+			output += '<a href="' + post.URL  + '">';
 			output += '<h4>' + post.title + '</h4>';
 			output += '</a>';
 			output += post.excerpt;
