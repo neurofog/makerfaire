929dd72ca38a797533b72ea7dc56ab2b25eb522f
Bill Olson <bolson@makermedia.com>
2014-07-28 16:48:55 -0700

    Maker profile updates.

--- plugins/maker-faire-forms/forms/makerprofile.php
+++ plugins/maker-faire-forms/forms/makerprofile.php
@@ -7,7 +7,16 @@
         <a href="#" onclick="gigya.accounts.showScreenSet({screenSet:'MakerFaire-Profile'});" />Edit Profile</a>
     </div>
 
-	<p><a href="<?php echo esc_url( home_url( '/new-york-2014/call-for-makers/' ) ); ?>"><input type="button" value="Apply for Maker Faire" /></a></p>
+<div class="clear"></div><br /><br />
+        <p><strong>The deadline for Maker entries has passed.</strong> However, there are several ways that you can still participate!:</p>
+
+        <ol>
+            <li><strong>We will consider your entry as a last-minute addition.</strong> Apply late by clicking the red button below. If you do not receive an acceptance letter by September 5, we were not able to find space for your exhibit. We will do our best to notify you before then.</li>
+            <li>If you would like to volunteer your time and make an invaluable contribution to the success of Maker Faire, please sign up for our <a href="http://makerfaire.com/new-york-2014/traveler-program/">Maker Faire Traveler Program</a>, which is a platform to enhance your skills and learn about the Maker Movement. You will have a behind-the-scenes experience, and help make the Greatest Show (and Tell) on Earth happen! <a href="http://makerfaire.com/new-york-2014/traveler-program/">Learn more here</a>.</li>
+            <li>Plan to come as an attendee, enjoy the show and support the Maker movement by <a href="https://makerfaireny2014.eventbrite.com/" target="_blank">purchasing your tickets early!</a></li>
+        </ol>
+
+         <p><a href="<?php echo esc_url( home_url( '/new-york-2014/call-for-makers/' ) ); ?>"><input type="button" value="Apply for Maker Faire" /></a></p>
 
     <div class="loading">
         <p><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/ajax-loader.gif" alt=""> Loading Applications...</p>
